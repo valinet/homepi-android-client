0c3e17939e25985a1aa18489b6392224fc034cd6
Valentin-Gabriel Radu <vali@valinet.ro>
2020-11-16 03:16:52 +0200

    Updated code * Fixed a few quirks, now it works without any weird hacks because homepi   now has a proper DNS-SD implementation * Updated API level.

--- app/src/main/java/com/valinet/homepi/MainActivity.java
+++ app/src/main/java/com/valinet/homepi/MainActivity.java
@@ -27,16 +27,17 @@ public class MainActivity extends AppCompatActivity {
     String ips;
     private NsdManager mNsdManager;
     private String SERVICE_NAME = "Client Device";
-    private String SERVICE_TYPE = "_workstation._tcp.";
-    private static final String TAG = "BonjourManager";
+    private String SERVICE_TYPE = "_valinet._tcp.";
     WifiManager.MulticastLock multicastLock = null;
     SwipeRefreshLayout mySwipeRefreshLayout;
     WebView view;
-    HashMap<String, String> hosts = new HashMap<String, String>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        findViewById(android.R.id.content).getRootView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        findViewById(android.R.id.content).getRootView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
 
         super.onCreate(savedInstanceState);
 
@@ -47,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         catch (NullPointerException e){}
 
         setContentView(R.layout.activity_main);
-
         mySwipeRefreshLayout = (SwipeRefreshLayout)this.findViewById(R.id.swipeContainer);
         mySwipeRefreshLayout.setRefreshing(true);
         view=(WebView) this.findViewById(R.id.webview);
@@ -63,35 +63,6 @@ public class MainActivity extends AppCompatActivity {
         });
         view.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading (WebView view,
-                                                     WebResourceRequest request)
-            {
-                Uri url = request.getUrl();
-                Log.e(TAG, "url: " + url.getHost().toString());
-                if (hosts.containsKey(url.getHost().toString()))
-                {
-                    URL originalURL = null;
-                    try {
-                        originalURL = new URL(url.toString());
-                        try {
-                            URL newURL = new URL(
-                                    originalURL.getProtocol(),
-                                    hosts.get(url.getHost().toString()),
-                                    originalURL.getPort(),
-                                    originalURL.getFile()
-                            );
-                            view.loadUrl(newURL.toString());
-                            return true;
-                        } catch (MalformedURLException e) {
-                            e.printStackTrace();
-                        }
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return false;
-            }
-            @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 mySwipeRefreshLayout.setRefreshing(false);
@@ -105,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
                         "})()");
             }
         });
-        //view.loadUrl(url);
 
         mySwipeRefreshLayout.setOnRefreshListener(
             new SwipeRefreshLayout.OnRefreshListener() {
@@ -116,71 +86,50 @@ public class MainActivity extends AppCompatActivity {
             }
         );
 
-        WifiManager wifi = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiManager wifi = (WifiManager)getApplicationContext().getSystemService(
+                Context.WIFI_SERVICE
+        );
         multicastLock = wifi.createMulticastLock("multicastLock");
         multicastLock.setReferenceCounted(true);
         multicastLock.acquire();
 
         mNsdManager = (NsdManager)getSystemService(Context.NSD_SERVICE);
-        mNsdManager.discoverServices(SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
+        mNsdManager.discoverServices(
+                SERVICE_TYPE,
+                NsdManager.PROTOCOL_DNS_SD,
+                mDiscoveryListener
+        );
     }
 
-    NsdManager.DiscoveryListener mDiscoveryListener = new NsdManager.DiscoveryListener() {
-
+    NsdManager.DiscoveryListener mDiscoveryListener = new NsdManager.DiscoveryListener()
+    {
         // Called as soon as service discovery begins.
         @Override
         public void onDiscoveryStarted(String regType) {
-            Log.d(TAG, "Service discovery started");
         }
 
         @Override
         public void onServiceFound(NsdServiceInfo service) {
-            // A service was found! Do something with it.
-            //Log.d(TAG, "Service discovery success : " + service);
-            //Log.d(TAG, "Host = "+ service.getHost());
-            //Log.d(TAG, "port = " + String.valueOf(service.getPort()));
-
-            if (!service.getServiceType().equals(SERVICE_TYPE)) {
-                // Service type is the string containing the protocol and
-                // transport layer for this service.
-                Log.d(TAG, "Unknown Service Type: " + service.getServiceType());
-            } else if (service.getServiceName().equals(SERVICE_NAME)) {
-                // The name of the service tells the user what they'd be
-                // connecting to. It could be "Bob's Chat App".
-                Log.d(TAG, "Same machine: " + SERVICE_NAME);
-            } else {
-                Log.d(TAG, "Found service");
-
-                // connect to the service and obtain serviceInfo
+            if (service.getServiceType().equals(SERVICE_TYPE)) {
                 mNsdManager.resolveService(service, new NsdManager.ResolveListener() {
                     @Override
                     public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
-                        // Called when the resolve fails.  Use the error code to debug.
-                        Log.e(TAG, "Resolve failed" + errorCode);
                     }
 
                     @Override
                     public void onServiceResolved(NsdServiceInfo serviceInfo) {
-                        Log.e(TAG, "Resolve Succeeded. " + serviceInfo);
-
                         if (serviceInfo.getServiceName().equals(SERVICE_NAME)) {
-                            Log.d(TAG, "Same IP.");
                             return;
                         }
 
-                        int port = serviceInfo.getPort();
-                        InetAddress host = serviceInfo.getHost(); // getHost() will work now
-
-                        final String addr = serviceInfo.getHost().toString().replace("/", "");
-                        Log.e(TAG, "Added: " + serviceInfo.getServiceName().toString().split(" ")[0] + ".local");
-                        hosts.put(serviceInfo.getServiceName().toString().split(" ")[0] + ".local", addr);
+                        final int port = serviceInfo.getPort();
+                        final InetAddress host = serviceInfo.getHost(); // getHost() will work now
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                //WebView myWebView = (WebView) findViewById(R.id.webview);
-                                //myWebView.setWebViewClient(new WebViewClient());
-                                view.loadUrl("http://" + addr);
-                                ips = addr;
+                                if (port == 80) {
+                                    view.loadUrl("http://" + host.toString());
+                                }
                             }
                         });
                     }
@@ -189,15 +138,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onServiceLost(NsdServiceInfo service) {
-            // When the network service is no longer available.
-            // Internal bookkeeping code goes here.
-            Log.e(TAG, "service lost" + service);
+        public void onServiceLost(NsdServiceInfo service)
+        {
         }
 
         @Override
-        public void onDiscoveryStopped(String serviceType) {
-            Log.i(TAG, "Discovery stopped: " + serviceType);
+        public void onDiscoveryStopped(String serviceType)
+        {
             if (multicastLock != null) {
                 multicastLock.release();
                 multicastLock = null;
@@ -205,8 +152,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onStartDiscoveryFailed(String serviceType, int errorCode) {
-            Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+        public void onStartDiscoveryFailed(String serviceType, int errorCode)
+        {
             mNsdManager.stopServiceDiscovery(this);
             if (multicastLock != null) {
                 multicastLock.release();
@@ -215,8 +162,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onStopDiscoveryFailed(String serviceType, int errorCode) {
-            Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+        public void onStopDiscoveryFailed(String serviceType, int errorCode)
+        {
             mNsdManager.stopServiceDiscovery(this);
             if (multicastLock != null) {
                 multicastLock.release();
